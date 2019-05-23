00add3b8c1c07fc55b89a7c98f610def317ec1bb
Jason  Montes <jasone.montes@gmail.com>
2019-05-23 23:45:14 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,40 +1,40 @@
 
-class Author
+# class Author
   
   
-  attr_accessor :name , :posts
+#   attr_accessor :name , :posts
   
-  @@all = []
+#   @@all = []
   
-  def initialize(name)
-    @name = name
-    @posts = []
-  end
+#   def initialize(name)
+#     @name = name
+#     @posts = []
+#   end
   
-  def add_post(post)
-    self.posts << post
-    post.author = self
-  end
+#   def add_post(post)
+#     self.posts << post
+#     post.author = self
+#   end
   
-  def posts
-    @posts
-  end 
+#   def posts
+#     @posts
+#   end 
   
-  def add_post_by_title(title)
-    add_post(Post.new(title))
-    # post = Post.new(title)
-    # @posts << post
-    # @@all << post 
-    # post.author = self
-  end 
+#   def add_post_by_title(title)
+#     add_post(Post.new(title))
+#     # post = Post.new(title)
+#     # @posts << post
+#     # @@all << post 
+#     # post.author = self
+#   end 
   
 
-def self.post_count
-    @@all.length
-  end
-end
-  # def self.post_count
-  #   @@all.length
-  # end
+# def self.post_count
+#     @@all.length
+#   end
+# end
+#   # def self.post_count
+#   #   @@all.length
+#   # end
   
 
